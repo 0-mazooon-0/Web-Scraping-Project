581569c5820a1e4e310a77c421c736e60d626d56
mazen_ashraf101 <163750604+0-mazooon-0@users.noreply.github.com>
2025-08-11 16:12:03 +0300

    Update and rename OOP Script.py to Script.py

--- Script.py
+++ Script.py
@@ -5,48 +5,56 @@ import selenium.common.exceptions as exceptions
 import urllib
 import os
 import time as t
-# //*[@id="rso"]/div
+
 
 class Scraping_Images:
-    def __init__(self, topic : str,*, download_path : str = None):
-     
+
+    def __init__(self, topic: str, *, download_path: str = None):
+
         if len(topic) < 1 or type(topic) != str:
-            raise ValueError("Your topic is too short or invalid please try another one.\n")
+            raise ValueError(
+                "Your topic is too short or invalid please try another one.\n")
         else:
             self.topic = topic
 
         if download_path != None:
             if os.path.isdir(os.path.dirname(os.path.abspath(download_path))):
                 path = download_path + f'\{topic}'
-                os.makedirs(path, exist_ok = True)
+                os.makedirs(path, exist_ok=True)
                 self.download_path = path
             else:
-                raise ValueError("Your driver address is invalid or typed incorrectly please try again.\n")
+                raise ValueError(
+                    "Your driver address is invalid or typed incorrectly please try again.\n"
+                )
 
         else:
             path = os.path.dirname(os.path.abspath(__file__))
             path += f'\{topic}'
-            os.makedirs(path, exist_ok = True)
+            os.makedirs(path, exist_ok=True)
             self.download_path = path
 
     def scrape(self):
 
         def download_image(url, filename):
             resource = urllib.request.urlopen(url)
-            output = open(filename,"wb")
+            output = open(filename, "wb")
             output.write(resource.read())
             output.close()
 
         self.__window = webdriver.Chrome()
-        self.__window.get(f"https://www.google.com/search?sxsrf=AE3TifOzKmAS32r4SRbjNst0HfZz4wyVsw:1754136543040&udm=2&q={self.topic}")
-        
+        self.__window.get(
+            f"https://www.google.com/search?sxsrf=AE3TifOzKmAS32r4SRbjNst0HfZz4wyVsw:1754136543040&udm=2&q={self.topic}"
+        )
 
-        self.__main_code = self.__window.find_element(By.XPATH, '//*[@id="rso"]/div')
+        self.__main_code = self.__window.find_element(By.XPATH,
+                                                      '//*[@id="rso"]/div')
 
         last_height = 0
         while 1:
-            new_height = self.__window.execute_script("return document.body.scrollHeight")
-            self.__window.execute_script(f"window.scrollTo({last_height}, document.body.scrollHeight);")
+            new_height = self.__window.execute_script(
+                "return document.body.scrollHeight")
+            self.__window.execute_script(
+                f"window.scrollTo({last_height}, document.body.scrollHeight);")
             t.sleep(2)
 
             if last_height == new_height:
@@ -54,11 +62,14 @@ class Scraping_Images:
             else:
                 last_height = new_height
 
-        self.__photo_tags = self.__main_code.find_elements(By.XPATH, '//*[@id="rso"]/div/div/div[1]/div/div/div/div[2]/h3/a/div/div/div/g-img/img')
+        self.__photo_tags = self.__main_code.find_elements(
+            By.XPATH,
+            '//*[@id="rso"]/div/div/div[1]/div/div/div/div[2]/h3/a/div/div/div/g-img/img'
+        )
         self.__photo_src = []
 
         for x in self.__photo_tags:
-            self.__photo_src.append( x.get_attribute('src') )
+            self.__photo_src.append(x.get_attribute('src'))
 
         self.__photos = 1
         for x in self.__photo_src:
@@ -69,8 +80,10 @@ class Scraping_Images:
             self.__photos += 1
 
         else:
-            print(f"\nYour images are downloaded successfully.\nYou can find your photos at {self.download_path}\nThanks for your time <3 <3 !!\n")
-    
+            print(
+                f"\nYour images are downloaded successfully.\nYou can find your photos at {self.download_path}\nThanks for your time <3 <3 !!\n"
+            )
+
     def photo_count(self):
         try:
             print(f"There are {self.__photos} photos")
@@ -80,8 +93,16 @@ class Scraping_Images:
             print(exc)
 
 
-myscrape1 = Scraping_Images('roberto carlos')
-# myscrape2 = Scraping_Images('Messi',download_path = r"C:\Users\mazen ashraf\Desktop")
+# You can try this
+# myscrape1 = Scraping_Images('roberto carlos')
 
-myscrape1.scrape()
-myscrape1.photo_count()
+# myscrape1.scrape()
+# myscrape1.photo_count()
+
+# You can try this
+# # myscrape2 = Scraping_Images('Messi',download_path = r"C:\Users\mazen ashraf\Desktop")
+
+# myscrape2.scrape()
+# myscrape2.photo_count()
+
+
